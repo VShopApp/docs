aa8ae3eaee38cbe6a924f97550fb848366ae0f26
vasc <nico@vasc.dev>
2025-02-26 21:27:48 +0100

    improve styles, add plausible & bump

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,20 +5,39 @@ import tailwind from "@astrojs/tailwind";
 import { rehypeHeadingIds } from "@astrojs/markdown-remark";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
+const { PLAUSIBLE_URL, PLAUSIBLE_DOMAIN } = process.env;
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://docs.vshop.one",
   integrations: [
     starlight({
       title: "VShop Docs",
-      customCss: ["./src/styles/tailwind.css", "./src/styles/headings.css"],
-      favicon: "./src/assets/logo.svg",
+      customCss: ["./src/styles/tailwind.css", "./src/styles/custom.css"],
+      favicon: "./src/assets/logo-white.svg",
       logo: {
-        src: "./src/assets/logo.svg",
+        dark: "./src/assets/logo-white.svg",
+        light: "./src/assets/logo-black.svg",
       },
       social: {
         github: "https://github.com/vshopapp/docs",
       },
+      editLink: {
+        baseUrl: "https://github.com/vshopapp/docs/edit/main",
+      },
+      head:
+        PLAUSIBLE_URL && PLAUSIBLE_DOMAIN
+          ? [
+              {
+                tag: "script",
+                attrs: {
+                  src: `${PLAUSIBLE_URL}/js/script.hash.outbound-links.js`,
+                  "data-domain": PLAUSIBLE_DOMAIN,
+                  defer: true,
+                },
+              },
+            ]
+          : undefined,
     }),
     tailwind({
       applyBaseStyles: false,
